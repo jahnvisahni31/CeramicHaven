a1e65a9152d43a18c23a12482da2eee025f67b8b
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-07-10 07:10:23 +0000

    new

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar"; // Assuming Navbar component file is in the same directory
+import "./index.css"; // Import global styles
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
